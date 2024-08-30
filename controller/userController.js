9d446a1e27aac0db482b1f74eb58627018ab2239
Joginder1706 <130969250+Joginder1706@users.noreply.github.com>
2024-08-30 10:04:42 +0530

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -3,30 +3,28 @@ import db from "../db/db.js";
 export const getUserById = (req, res) => {
   const id = req.params.id;
   // console.log(id);
-//   const query = `
-//   SELECT DISTINCT
-//     users.first_name, 
-//     users.city_name, 
-//     users.gender, 
-//     users.created, 
-//     users.active_date,
-//     places.logo, 
-//     places.description,
-//     places.characters_description,
-//     places.short_desc, 
-//     places.details, 
-//     places.healthtest_date
-//   FROM users 
-//   LEFT JOIN places 
-//   ON users.id = places.userid 
-//   WHERE users.id = ${id}
-// `;
-
-  const query = `SELECT * FROM wp_users`;
+  const query = `
+  SELECT DISTINCT
+    users.first_name, 
+    users.city_name, 
+    users.gender, 
+    users.created, 
+    users.active_date,
+    places.logo, 
+    places.description,
+    places.characters_description,
+    places.short_desc, 
+    places.details, 
+    places.healthtest_date
+  FROM users 
+  LEFT JOIN places 
+  ON users.id = places.userid 
+  WHERE users.id = ${id}
+`;
 
   db.query(query, (err, results) => {
     if (err) {
-      return res.status(500).json({ error: err });
+      return res.status(500).json({ error: err.message });
     }
     return res.json(results);
   });
